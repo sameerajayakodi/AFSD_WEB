5122624546549fdb4ac062d85c3376e653798f69
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-11-03 12:22:40 +0530

    Create common.js

---
+++ JS_Modules/common.js
@@ -0,0 +1,13 @@
+export function getName() {
+  console.log("name is John");
+}
+
+export function getAge() {
+  console.log("age is 20");
+}
+
+export let age = 20;
+
+export default function defaultExport() {
+  console.log("I am the default export");
+}
