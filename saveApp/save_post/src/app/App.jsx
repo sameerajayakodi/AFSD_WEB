3d3ae60a6ba19d247bbb67b1ef1c5a237b6ec8a1
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-11-21 13:42:38 +0530

    Create App.jsx

---
+++ saveApp/save_post/src/app/App.jsx
@@ -0,0 +1,18 @@
+
+import Card from '../common/components/Card/Card';
+import './App.css';
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className='main'>
+    <Card cardtitle="Save Post" />
+    </div>
+      
+    </>
+  );
+}
+
+export default App
