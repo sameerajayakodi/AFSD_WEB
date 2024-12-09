fa56900bc5385a202988e034e524a43401e612f7
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-12-09 16:26:44 +0530

    Update App.jsx

--- React/vite-project/src/app/App.jsx
+++ React/vite-project/src/app/App.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import DrawerPage from "../pages/DrawerPage/DrawerPage";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import RegisterPage from "../pages/RegisterPage/RegisterPage";
 import "./App.css";
 
 function App() {
+  const [login, setLogin] = useState(false);
+
+  useEffect(() => {
+    console.log(localStorage.getItem("afsd-token"));
+    if (localStorage.getItem("afsd-token")) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  }, []);
   return (
     <>
+      {login ? (
+        <DrawerPage />
+      ) : (
+        <Routes>
+          {/*sampurna page ekak wens weddi routing app.jsx hari componet ekaka hari danna puluwan */}
+          {/* router eka hadnn oni e adala component eka load wenna ona thana */}
+          {/* default route eka widiyata * ho / denna puluwan ...Navigate use karala redirect karanna puluwan */}
+
+          <Route
+            path="*"
+            element={<Navigate to={"/login"} key={"navigate"} />}
+          />
+          <Route path="/login" element={<LoginPage key={"login"} />} />
+          <Route path="/register" element={<RegisterPage key={"register"} />} />
+        </Routes>
+      )}
       {/* <Home />
       <About /> */}
       {/* <MuiPage /> */}
@@ -13,16 +41,6 @@ function App() {
       {/* <AxiosPage /> */}
       {/* <LoginPage />
       <RegisterPage /> */}
-
-      <Routes>
-        {/*sampurna page ekak wens weddi routing app.jsx hari componet ekaka hari danna puluwan */}
-        {/* router eka hadnn oni e adala component eka load wenna ona thana */}
-        {/* default route eka widiyata * ho / denna puluwan ...Navigate use karala redirect karanna puluwan */}
-
-        <Route path="*" element={<Navigate to={"/login"} />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
     </>
   );
 }
