38225dcff1e20f9a477403d3f5828a6343cb3120
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-12-09 16:26:32 +0530

    Update Clear.jsx

--- paintApp/paint-app/src/components/Controls/Clear/Clear.jsx
+++ paintApp/paint-app/src/components/Controls/Clear/Clear.jsx
@@ -3,7 +3,11 @@ import Button from "@mui/material/Button";
 // eslint-disable-next-line react/prop-types
 export default function Clear({ onClear }) {
   return (
-    <Button style={{ width: "150px" }} onClick={onClear} variant="contained">
+    <Button
+      style={{ width: "150px", height: "50px" }}
+      onClick={onClear}
+      variant="contained"
+    >
       clean
     </Button>
   );
