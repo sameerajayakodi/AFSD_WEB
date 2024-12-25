6522360c9217aea5f62854176e6163bec56fbfb6
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-12-25 08:48:33 +0530

    Update ThemeProvider.jsx

--- first-project/src/common/components/ThemeProvider/ThemeProvider.jsx
+++ first-project/src/common/components/ThemeProvider/ThemeProvider.jsx
@@ -2,6 +2,7 @@ import { createContext, useState } from "react";
 
 const ThemeContext = createContext();
 
+// eslint-disable-next-line react/prop-types
 const ThemeProvider = ({children}) => {
 
     const [theme, setTheme] = useState('light');
